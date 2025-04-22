48f59e10b6ea411c91399e6122ea84919e1d68a0
Russell Davis <551404+russelldavis@users.noreply.github.com>
2025-04-21 22:31:40 -0700

    Only apply rules to main_frame

--- config.js
+++ config.js
@@ -27,21 +27,7 @@ export default {
         }
     },
     resourceTypes: [
-        "main_frame",
-        "sub_frame",
-        "stylesheet",
-        "script",
-        "image",
-        "font",
-        "object",
-        "xmlhttprequest",
-        "ping",
-        "csp_report",
-        "media",
-        "websocket",
-        "webtransport",
-        "webbundle",
-        "other"
+        "main_frame"
     ],
     highlightedTokens: [
         "allow",
